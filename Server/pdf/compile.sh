430fc3062de4f6f40a6dd3a22dc6372eea3347eb
Zekun Zhang <kzz1994@gmail.com>
2015-01-19 18:13:16 +0800

    Update

--- Server/pdf/compile.sh
+++ Server/pdf/compile.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-cd ./pdf/								# go to working dir
-pdflatex main.tex						# compile
-rm main.aux main.log					# remove useless files
+cd ./pdf/	# go to working dir
+pdflatex main.tex	# compile
+rm main.aux main.log	# remove useless files
 sudo cp main.pdf /var/www/PDF/list.pdf	# copy PDF to HTTP dir
