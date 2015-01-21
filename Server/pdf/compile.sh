f6ae4c1401328f8f921e1bd506f81cb61c386850
Zekun Zhang <kzz1994@gmail.com>
2015-01-21 10:05:17 +0800

    fix HTTP directory

--- Server/pdf/compile.sh
+++ Server/pdf/compile.sh
@@ -3,4 +3,6 @@
 cd ./pdf/	# go to working directory
 pdflatex main.tex	# compile
 rm main.aux main.log	# remove useless files
-sudo cp main.pdf /var/www/PDF/list.pdf	# copy PDF to HTTP directory
+WWW_DIR=$(cat /etc/passwd | grep -i 'www' | cut -d ':' -f 6)	# get HTTP directory
+sudo mkdir -p ${WWW_DIR}/PDF
+sudo cp main.pdf ${WWW_DIR}/PDF/list.pdf	# copy PDF to HTTP directory
